3861c9ba0d88716b6bd8c99db536427df312c2ed
drewp <drewp@bigasterisk.com>
2009-07-06 21:38:55 -0700

    add command class ping to dbclient, fix up unittests
    Ignore-this: 8753abe536e34cee283f8877c39d564d
    
    darcs-hash:20090707043855-312f9-3a46ea1aef5ca4436a0630c4e2841ddf15845e9b.gz

--- commandinference/test_db.py
+++ commandinference/test_db.py
@@ -1,11 +1,15 @@
 from twisted.trial import unittest
 from rdflib import Namespace, Literal, RDF
-from rdflib.Graph import Graph
+from rdflib.Graph import ConjunctiveGraph
 from db import CommandLog
+from sparqlhttp.dictquery import Graph2
 
 CMD = Namespace("http://example.com/")
 USER = Namespace("http://example.com/user/")
 XS = Namespace("http://www.w3.org/2001/XMLSchema#")
+DCTERMS = Namespace("http://purl.org/dc/terms/")
+CL = Namespace("http://bigasterisk.com/ns/command/v1#")
+INITNS = dict(cl=CL, dcterms=DCTERMS)
 
 def dateTime(timeStr):
     return Literal("2008-12-01T%s-08:00" % timeStr, datatype=XS['dateTime'])
@@ -13,9 +17,9 @@ def dateTime(timeStr):
 class WithGraph(object):
     def setUp(self):
         super(WithGraph, self).setUp()
-        self.graph = Graph()
+        self.graph = ConjunctiveGraph()
         self.graph.add((CMD['c2'], RDF.type, CMD['Bright']))
-        self.cl = CommandLog(self.graph)
+        self.cl = CommandLog(Graph2(self.graph, initNs=INITNS))
 
 class TestLogAdd(WithGraph, unittest.TestCase):
     def testNormalAdd(self):
@@ -31,15 +35,15 @@ class TestLogAdd(WithGraph, unittest.TestCase):
 
 class TestCommandLogGraphInit(unittest.TestCase):
     def testUsesSingleGraphForRW(self):
-        g1 = Graph()
-        c = CommandLog(g1)
+        g1 = ConjunctiveGraph()
+        c = CommandLog(Graph2(g1, initNs=INITNS))
         c.addCommand(CMD['c1'], dateTime('18:00:00'), USER['drewp'])
         self.assert_(len(g1) > 0)
 
     def testWritesToSeparateGraph(self):
-        g1 = Graph()
-        g2 = Graph()
-        c = CommandLog(g1, g2)
+        g1 = ConjunctiveGraph()
+        g2 = ConjunctiveGraph()
+        c = CommandLog(Graph2(g1, initNs=INITNS), Graph2(g2, initNs=INITNS))
         c.addCommand(CMD['c1'], dateTime('18:00:00'), USER['drewp'])
         self.assertEqual(len(g1), 0)
         self.assert_(len(g2) > 0)
@@ -73,6 +77,39 @@ class TestLogCustomQuery(WithGraph, unittest.TestCase):
     def testLastCommandNoData(self):
         self.assertRaises(ValueError,
                           self.cl.lastCommandOfClass, CMD['Nonexist'])
+        
+
+class TestPing(WithGraph, unittest.TestCase):
+    def testSendsPing(self):
+        pings = []
+        
+        self.cl = CommandLog(Graph2(self.graph, initNs=INITNS),
+                             newCommandPing=lambda **kw: pings.append(kw))
+        self.cl.addCommand(CMD['c2'], dateTime('18:00:00'), USER['drewp'])
+        self.assertEqual(pings, [
+            {'content': 'http://bigasterisk.com/command/http%3A//example.com/c2/1228183200.0/http%3A//example.com/user/drewp',
+             'signal': 'http://example.com/Bright'}])
+
+    def testSendsMultipleClasses(self):
+        pings = []
+        
+        self.cl = CommandLog(Graph2(self.graph, initNs=INITNS),
+                             newCommandPing=lambda **kw: pings.append(kw))
+        self.graph.add((CMD['c2'], RDF.type, CMD['Volume'])) # second cls
+
+        self.cl.addCommand(CMD['c2'], dateTime('18:00:00'), USER['drewp'])
+        c = 'http://bigasterisk.com/command/http%3A//example.com/c2/1228183200.0/http%3A//example.com/user/drewp'
+        self.assertEqual(sorted(pings), sorted([
+            {'content': c, 'signal': 'http://example.com/Bright'},
+            {'content': c, 'signal': 'http://example.com/Volume'}]))
+
+    def testDoesntSendStrayClasses(self):
+        pings = []
+        self.graph.add((CMD['other'], RDF.type, CMD['Other']))
+        self.cl = CommandLog(Graph2(self.graph, initNs=INITNS),
+                             newCommandPing=lambda **kw: pings.append(kw))
+        self.cl.addCommand(CMD['c2'], dateTime('18:00:00'), USER['drewp'])
+        self.assertEqual(len(pings), 1)
         
 
 # todo: times in different zones probably don't sort correctly unless
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all: 
 	curl -T auth.n3 -H "Content-Type: text/rdf+n3" "http://plus:8080/openrdf-sesame/repositories/cmd/statements?context=%3Chttp%3A%2F%2Fbigasterisk.com%2Frdf%2Fauth1%3E"
+
+test:
+	(cd commandinference; ../buildout_bin/py `which trial` test_db)
