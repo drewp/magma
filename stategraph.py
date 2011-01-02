0032de3534b0c23ece26821307cb3329152c054b
drewp@bigasterisk.com <drewp@bigasterisk.com>
2011-01-02 08:29:50 -0800

    stategraph helper class Ignore-this: e3ef4e2fa2b196355bcedc89e1986150

---
+++ stategraph.py
@@ -0,0 +1,27 @@
+import datetime, os
+from dateutil.tz import tzlocal
+from rdflib.Graph import Graph
+from rdflib import Namespace, Literal
+DCTERMS = Namespace("http://purl.org/dc/terms/")
+
+class StateGraph(object):
+    """
+    helper to create a graph with some of the current state of the world
+    """
+    def __init__(self, ctx):
+        """
+        note that we put the time of the __init__ call into the graph
+        as its dcterms:modified time.
+        """
+        self.g = Graph()
+        self.ctx = ctx
+        self.g.add((ctx, DCTERMS['creator'],
+                    Literal(os.path.abspath(__file__))))
+        self.g.add((ctx, DCTERMS['modified'],
+               Literal(datetime.datetime.now(tzlocal()))))
+
+    def add(self, *args, **kw):
+        self.g.add(*args, **kw)
+
+    def asTrig(self):
+        return "%s {\n%s\n}\n" % (self.ctx.n3(), self.g.serialize(format='nt'))
