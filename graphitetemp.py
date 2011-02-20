77de3a4a76f7615c924e5d8ab96eeaa9f0f73d8d
drewp@bigasterisk.com <drewp@bigasterisk.com>
2011-02-20 01:08:38 -0800

    new temperature section in progress Ignore-this: b60bd927e62da42844a59b927e0831ce

--- graphitetemp.py
+++ graphitetemp.py
@@ -1,19 +1,27 @@
 import restkit, logging, jsonlib
 log = logging.getLogger()
 
+def _graphiteGet(graphite, target):
+    data = graphite.get('',
+                        target="keepLastValue(%s)" % target,
+                        rawData='true',
+                        **{'from':'-10minutes'}).body_string()
+    v = data.split(',')[-1]
+    return float(v)
+
 def getAllTemps():
     lastTemp = {}
     graphite = restkit.Resource("http://graphite.bigasterisk.com/render/")
     for name in ['downstairs', 'bedroom', 'livingRoom']:
-        data = graphite.get('',
-                            target="keepLastValue(system.house.temp.%s)" % name,
-                            rawData='true',
-                            **{'from':'-10minutes'}).body_string()
-        v = data.split(',')[-1]
         try:
-            lastTemp[name] = float(v)
+            lastTemp[name] = _graphiteGet(graphite, "system.house.temp.%s" % name)
         except ValueError:
-            log.error("temp failed: %r -> %r" % (name, v))
+            log.error("temp failed: %r" % name)
+
+    try:
+        lastTemp['KSQL'] = _graphiteGet(graphite, "system.noaa.ksql.temp_f")
+    except ValueError:
+        pass
 
     lastTemp['ariBedroom'] = jsonlib.read(
         restkit.request("http://star:9014/temperature").body_string(),
