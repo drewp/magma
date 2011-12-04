30eba5297651ef71e777a7bd959398c2a682924b
drewp <drewp@bigasterisk.com>
2011-12-03 20:36:28 -0800

    work around restkit returning empty docs
    Ignore-this: 77da7ee6b7e36d2729ce1bc943479b8e
    
    darcs-hash:20111204043628-312f9-7392a185d71af3e74ebb58130acda48fbaec6f33.gz

--- graphitetemp.py
+++ graphitetemp.py
@@ -1,26 +1,33 @@
-import restkit, logging, jsonlib
+import restkit, logging, jsonlib, sys, urllib
 log = logging.getLogger()
 
-def _graphiteGet(graphite, target):
-    data = graphite.get('',
-                        target="keepLastValue(%s)" % target,
-                        rawData='true',
-                        **{'from':'-60minutes'}).body_string()
+def httpGet(url, params):
+    # restkit 3.3.2 returns empty string for this!
+    return urllib.urlopen(url+"?"+urllib.urlencode(params)).read()
+
+def _graphiteGet(target):
+    # "http://graphite.bigasterisk.com/render/" normal url, but it logs requests
+    data = httpGet("http://bang:9037/render", {
+        'target':"keepLastValue(%s)" % target,
+        'rawData':'true',
+        'from':'-60minutes',
+        })
+    #headers={"user-agent":"%s _graphiteGet" % sys.argv[0]}, # sent this when using restkit
+
     v = data.split('|')[-1].split(',')[-1]
     return float(v)
 
 def getAllTemps():
     lastTemp = {}
-    graphite = restkit.Resource("http://graphite.bigasterisk.com/render/")
     for name in ['downstairs', 'bedroom', 'livingRoom', 'ariBedroom', 'frontDoor']:
         try:
-            lastTemp[name] = _graphiteGet(graphite, "system.house.temp.%s" % 
+            lastTemp[name] = _graphiteGet("system.house.temp.%s" % 
                                           name.replace('ariBedroom', 'ariroom'))
         except ValueError:
             log.error("temp failed: %r" % name)
 
     try:
-        lastTemp['KSQL'] = _graphiteGet(graphite, "system.noaa.ksql.temp_f")
+        lastTemp['KSQL'] = _graphiteGet("system.noaa.ksql.temp_f")
     except ValueError:
         pass
 
