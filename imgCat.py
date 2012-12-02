396e97804142cab0631d9ee2c940d51a00d2ce33
drewp <drewp@bigasterisk.com>
2012-12-02 14:46:22 -0800

    port to bottle+gunicorn for concurrency
    Ignore-this: 9606bc60fe5ea38c9e127dc08c621b7b
    
    darcs-hash:20121202224622-312f9-bbcc24f608620b390ccc1f47c0eec7c49feaddb0

--- imgCat.py
+++ imgCat.py
@@ -2,14 +2,16 @@
 """
 proxy that serves multiple images as one concatenated one
 """
-import cyclone.web, restkit, urllib
+import bottle, restkit, urllib
 import json, Image
 from twisted.internet import reactor
 from StringIO import StringIO
 
-class Index(cyclone.web.RequestHandler):
-    def get(self):
-        self.write("imgcat: %r" % self.settings.config)
+config = json.loads(open("imgCat.conf").read())
+
+@bottle.route("/")
+def index():
+    return "imgcat: %r" % config
 
 def urlFromDescription(desc):
     params = []
@@ -20,40 +22,36 @@ def urlFromDescription(desc):
             params.append((k, v))
     return desc['uri'] + "?" + urllib.urlencode(params)
 
-class Cat(cyclone.web.RequestHandler):
-    def get(self, which):
-        desc = self.settings.config['output'][which]
+@bottle.route("/cat/:which")
+def cat(which):
+    desc = config['output'][which]
 
-        # should be parallel
-        imgs = []
-        for i in desc['inputs']:
-            if isinstance(i, dict):
-                i = urlFromDescription(i)
-            response = restkit.request(i, headers={
-                "Cookie" : self.request.headers.get('Cookie', '')
-                }, follow_redirect=True)
-            if response.status_int != 200:
-                raise ValueError("fetching %s -> %s" % (i, response.status))
-            data = response.body_string()
-            img = Image.open(StringIO(data))
-            if desc.get('resize', None) is not None:
-                img.thumbnail((32000, desc['resize']['height']), Image.ANTIALIAS)
-            imgs.append(img)
+    # should be parallel
+    imgs = []
+    for i in desc['inputs']:
+        if isinstance(i, dict):
+            i = urlFromDescription(i)
+        response = restkit.request(i, headers={
+            "Cookie" : bottle.request.headers.get('Cookie', '')
+            }, follow_redirect=True)
+        if response.status_int != 200:
+            raise ValueError("fetching %s -> %s" % (i, response.status))
+        data = response.body_string()
+        img = Image.open(StringIO(data))
+        if desc.get('resize', None) is not None:
+            img.thumbnail((32000, desc['resize']['height']), Image.ANTIALIAS)
+        imgs.append(img)
 
-        outSize = max(i.size[0] for i in imgs), sum(i.size[1] for i in imgs)
-        out = Image.new('RGB', outSize)
-        y = 0
-        for i in imgs:
-            out.paste(i, (0, y))
-            y = y + i.size[1]
+    outSize = max(i.size[0] for i in imgs), sum(i.size[1] for i in imgs)
+    out = Image.new('RGB', outSize)
+    y = 0
+    for i in imgs:
+        out.paste(i, (0, y))
+        y = y + i.size[1]
 
-        self.set_header("content-type", "image/jpeg" if not which.endswith('.png') else "image/png")
-        out.save(self, which.split('.')[-1].replace('jpg', 'jpeg'), **desc.get('saveOpts', {}))
+    bottle.response.set_header("content-type", "image/jpeg" if not which.endswith('.png') else "image/png")
+    save = StringIO()
+    out.save(save, which.split('.')[-1].replace('jpg', 'jpeg'), **desc.get('saveOpts', {}))
+    return save.getvalue()
 
-if __name__ == '__main__':
-    reactor.listenTCP(8013, cyclone.web.Application(
-        handlers=[(r"/", Index),
-                  (r"/cat/(.*)", Cat),
-                  ],
-        config=json.loads(open("imgCat.conf").read())))
-    reactor.run()
+bottle.run(server='gunicorn', host="0.0.0.0", port=8013, workers=4)
